d0505313d51b9f2d0fb1be295fcadee565b94908
Zachary Culpepper <zculp4579@gmail.com>
2025-07-27 08:59:18 -0500

    File Structure / Button UI / Scriptable Object(Question)

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text here";
+
+
+}
