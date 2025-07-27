5869671ebb5a6c9d50f353cb8f41d99f1e316082
Zachary Culpepper <zculp4579@gmail.com>
2025-07-27 11:59:08 -0500

    Developed Quiz logic / Added Timer.cs

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Quiz : MonoBehaviour
@@ -8,9 +9,49 @@ public class Quiz : MonoBehaviour
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] GameObject[] answerButtons;
-    
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
+    int correctAnswerIndex;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        DisplayQuestion();
+    }
+
+    public void OnAnswerSelect(int index)
+    {
+        Image buttonImage;
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct!";
+            
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was\n" + correctAnswer;
+
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    void DisplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -18,6 +59,22 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Button>().interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Image>().sprite = defaultAnswerSprite;
         }
     }
 
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    
+    public bool isAnsweringQuestion = false;
+
+    float timerValue;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion)
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+
+        Debug.Log(timerValue);
+    }
+}
